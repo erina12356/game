a682299d28206419b2646335df2498fb2f921e98
erina12356 <erinazymberi123@gmail.com>
2024-09-04 18:30:15 +0200

    game

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+choices = ["rock","paper","scissors"]
+
+def getcchoices():
+    return random.choice(choices)
+
+def pchoice():
+    print("choose rock paper or scissors")
+    playerinput = input("your choice")
+    while playerinput not in choices:
+        print("Invalid choice")
+        playerinput = input("your choice")
+    return playerinput
+
+def findwiner(playerinput, computerinput)
+    if playerinput == computerinput
+        return("tie")
+    elif(playerinput== "rock" and computerinput=="scissors") or (playerinput=="paper" and computerinput=="rock") or (playerinput=="paper" and computerinput=="rock"):
