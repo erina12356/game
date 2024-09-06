68fb75c57e982681051748cdea0be7819807f9fd
erina12356 <erinazymberi123@gmail.com>
2024-09-06 15:01:51 +0200

    game

--- main.py
+++ main.py
@@ -12,7 +12,19 @@ def pchoice():
         playerinput = input("your choice")
     return playerinput
 
-def findwiner(playerinput, computerinput)
-    if playerinput == computerinput
+def findwiner(playerinput, computerinput):
+    if playerinput == computerinput:
         return("tie")
-    elif(playerinput== "rock" and computerinput=="scissors") or (playerinput=="paper" and computerinput=="rock") or (playerinput=="paper" and computerinput=="rock"):
+    elif(playerinput== "rock" and computerinput=="scissors") or (playerinput=="paper" and computerinput=="rock") or (playerinput=="paper" and computerinput=="rock")or(playerinput=="scissors" and computerinput=="paper"):
+        return "you win"
+    else:
+        return "computer win"
+    
+
+def playgame():
+    computerinput = getcchoices()
+    playerinput  =  pchoice()
+    print("computer choice is", computerinput)
+    result=findwiner(playerinput, computerinput)
+    print(result)
+playgame()
